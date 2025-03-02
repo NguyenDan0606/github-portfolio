225d1b574f5080b6017dad67f280b3c9e4f674b9
Dan <heloemtrai1@gmail.com>
2025-03-02 23:20:49 +0700

    'push'

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,13 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-  leetcode: "https://leetcode.com/said3812/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  githubUser: 'NguyenDan0606',
+  devUsername: "NguyenDan0606",
+  github: 'https://github.com/NguyenDan0606',
+  facebook: 'https://www.facebook.com/nguyendan0606',
+  linkedIn: '',
+  twitter: '',
+  stackOverflow: '',
+  leetcode: "",
+  resume: "",
   skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
   timezone: '+6'
 };
